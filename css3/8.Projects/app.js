23f2aece3f7263ccbd7019395d5bae2dbdd20060
sankeerth-dev <sankeerthmansali1@gmail.com>
2025-11-20 18:14:35 +0530

    20-11-2025 contents

--- css3/8.Projects/app.js
+++ css3/8.Projects/app.js
@@ -5,3 +5,7 @@ setInterval( function()  {
     let id = Math.floor(Math.random() * 2)
     wBlk.innerText = works[id]
 }, 3000);
+
+// to update year with current year
+let yr = document.getElementById("yr");
+yr.innerText = new Date().getFullYear();
